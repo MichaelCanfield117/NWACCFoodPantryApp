d1bd7846ca81f9cb3ad5b40d7fe80074c432a796
wredwine <161385060+wredwine@users.noreply.github.com>
2024-02-26 23:41:51 -0600

    NWACC Food Pantry App with event handlers
    Added the ability to add, remove, and update items. Also added the Open File button that can import data from CSV files in a specified format.

---
+++ NWACCFoodPantryApp/InventoryItems.cs
@@ -0,0 +1,18 @@
+﻿// InventoryItems.cs
+
+namespace FoodPantryApp
+{
+    public class InventoryItems
+    {
+        public string Type { get; set; } 
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+
+        public InventoryItems(string type, string name, int quantity)
+        {
+            Type = type;
+            Name = name;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ NWACCFoodPantryApp/README.md
@@ -0,0 +1,2 @@
+# FoodPantryApp
+This is the GUI application for the Food Pantry Capstone Project for the NWACC Food Pantry of the Student Life Group.
