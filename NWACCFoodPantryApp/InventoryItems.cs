b86225d6df19866c2f755c4959f9da3d1226b0a4
wredwine <161385060+wredwine@users.noreply.github.com>
2024-02-26 20:26:43 -0600

    NWACC Food Pantry App
    Added the ability to add, remove, and update items.

---
+++ NWACCFoodPantryApp/InventoryItems.cs
@@ -0,0 +1,16 @@
+﻿// InventoryItems.cs
+
+namespace FoodPantryApp
+{
+    public class InventoryItems
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+
+        public InventoryItems(string name, int quantity)
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ NWACCFoodPantryApp/UpdateInventoryItemForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace FoodPantryApp
+{
+    partial class UpdateInventoryItemForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ItemNameTextBox2 = new TextBox();
+            SubmitButton2 = new Button();
+            CancelButton2 = new Button();
+            ItemQuantityTextBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // ItemNameTextBox2
+            // 
+            ItemNameTextBox2.Location = new Point(175, 108);
+            ItemNameTextBox2.Name = "ItemNameTextBox2";
+            ItemNameTextBox2.Size = new Size(211, 31);
+            ItemNameTextBox2.TabIndex = 0;
+            // 
+            // SubmitButton2
+            // 
+            SubmitButton2.Location = new Point(230, 173);
+            SubmitButton2.Name = "SubmitButton2";
+            SubmitButton2.Size = new Size(112, 34);
+            SubmitButton2.TabIndex = 1;
+            SubmitButton2.Text = "Submit";
+            SubmitButton2.UseVisualStyleBackColor = true;
+            SubmitButton2.Click += SubmitButton_Click;
+            // 
+            // CancelButton2
+            // 
+            CancelButton2.Location = new Point(401, 173);
+            CancelButton2.Name = "CancelButton2";
+            CancelButton2.Size = new Size(112, 34);
+            CancelButton2.TabIndex = 2;
+            CancelButton2.Text = "Cancel";
+            CancelButton2.UseVisualStyleBackColor = true;
+            CancelButton2.Click += CancelButton_Click;
+            // 
+            // ItemQuantityTextBox2
+            // 
+            ItemQuantityTextBox2.Location = new Point(423, 108);
+            ItemQuantityTextBox2.Name = "ItemQuantityTextBox2";
+            ItemQuantityTextBox2.Size = new Size(146, 31);
+            ItemQuantityTextBox2.TabIndex = 3;
+            // 
+            // UpdateInventoryItemForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ItemQuantityTextBox2);
+            Controls.Add(CancelButton2);
+            Controls.Add(SubmitButton2);
+            Controls.Add(ItemNameTextBox2);
+            Name = "UpdateInventoryItemForm";
+            Text = "UpdateInventoryItemForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox ItemNameTextBox2;
+        private Button SubmitButton2;
+        private Button CancelButton2;
+        private TextBox ItemQuantityTextBox2;
+    }
+}
